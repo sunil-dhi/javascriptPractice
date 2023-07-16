e773eb5c1aca216b4126a3235a103d26fc7d895e
sunil-dhi <sunildhimanmatharu@gmail.com>
2023-07-15 21:32:59 -0400

    generator func

---
+++ JsPractice2/generatorFun.js
@@ -0,0 +1,30 @@
+// const fun1=function*(){
+//     yield "a";
+//     yield "b";
+//     yield "c";
+// }
+
+// let str='';
+// for(let i of fun1()){
+//     str=str+i;
+// }
+// console.log(str)
+
+function *generator(){
+    yield 1;
+    yield 2;
+    yield 3;
+}
+let g=generator();
+// console.log([...g])   ///[1,2,3]
+
+console.log(g.next())               
+console.log(g.next())
+console.log(g.next())
+console.log(g.next())          
+
+
+// {value: 1, done: false}
+// {value: 2, done: false}
+//  {value: 3, done: false}
+// {value: undefined, done: true}
