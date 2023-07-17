3bf1d164d82395cee0213aa27c8359510dafe6cb
sunil-dhi <sunildhimanmatharu@gmail.com>
2023-07-16 22:45:23 -0400

    reverse method

--- JsPractice2/sortMethod.js
+++ JsPractice2/sortMethod.js
@@ -11,7 +11,14 @@ console.log(ar1.sort((a,b)=>{return b-a}))
 //gives output in descending order
 //output:[45, 21, 8, 5, 3, 3, 3, 2, 2, 1]
 
-let str=['banana','hello','sunil','sushila','sushili','sushilali'];
-console.log(str.sort())
+let str1=['banana','hello','sunil','sushila','sushili','sushilali'];
+console.log(str1.sort())
 
 // output:   ['banana', 'hello', 'sunil', 'sushila', 'sushilali', 'sushili']
+let str2=['banana','hello','sunil','sushila','sushili','sushilali'];
+let r=str2.sort()
+console.log(r.reverse())
+
+
+// output: using reverse method we can have descending ordered string
+// ['sushili', 'sushilali', 'sushila', 'sunil', 'hello', 'banana']
