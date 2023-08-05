15978091d1371c3e8f595cf5edaca6282df1882d
sunil-dhi <sunildhimanmatharu@gmail.com>
2023-08-05 09:32:56 -0400

    bind method js

---
+++ JsPractice2/bind.js
@@ -0,0 +1,15 @@
+// only diff betweeb call and bind is------>>>> bind method returns a function which is invoked later
+
+let students={
+    name:'sunil dhiman',
+    gender:'male',
+    age:18
+}
+
+function bindd(){
+    console.log(this.name+' '+this.gender +' '+this.age)
+}
+
+let res=bindd.bind(students)
+console.log(res)    //it gives a function as output to print value we need to call function again
+res()
