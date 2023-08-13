84a5c32bb8bc81cb1505481b91c8d5725e33d864
sunil-dhi <sunildhimanmatharu@gmail.com>
2023-08-13 09:41:35 -0400

    prouct of array except self in array

--- leetCodeJs/array_problem4.js
+++ leetCodeJs/array_problem4.js
@@ -1,21 +1,20 @@
+function selfprodcuct(arr) {
+  let resarr = [];
+  let product;
+  for (let i = 0; i < arr.length; i++) {
+    product = 1;
 
-function selfprodcuct(arr){
-let resarr=[];
-let product;
-for(let i=0;i<arr.length;i++){
-     product=1;
-
-    for(let j=0;j<arr.length;j++){
-        if(i!==j){
-            product*=arr[j]
-        }
-
+    for (let j = 0; j < arr.length; j++) {
+      if (i !== j) {
+        product *= arr[j];
+      }
     }
-    resarr.push(product)
-
+    resarr.push(product);
+  }
+  return resarr;
 }
-return resarr
+let a4 = [1, 2, 3, 4];
+console.log(selfprodcuct(a4));
 
-}
-let a4=[1,2,3,4];
-console.log(selfprodcuct(a4))
+
+// output:[24,12,8,6]
