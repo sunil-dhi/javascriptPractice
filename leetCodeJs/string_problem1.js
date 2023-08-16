aacecc8d6536ea4decbdcb5324c33ba57d3eb38a
sunil-dhi <sunildhimanmatharu@gmail.com>
2023-08-16 11:32:20 -0400

    //longest substring without repeating chars

---
+++ leetCodeJs/string_problem1.js
@@ -0,0 +1,21 @@
+var lengthOfLongestSubstring = function(s) {
+    let len=0;
+    let set=new Set();
+    let i=0;
+    let j=0;
+    while(i<s.length && j<s.length){
+        if(!set.has(s[j]))
+{
+    set.add(s[j]);
+    len=Math.max(len,j-i+1);
+    j++
+} 
+else{
+    set.delete(s[i]);
+    i++
+}
+   }
+   return len
+};
+
+console.log(lengthOfLongestSubstring('abcabcbb'))
