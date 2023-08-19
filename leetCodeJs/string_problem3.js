0cd56ed90bf37639d2af5cba6eabcbb891ed87d8
sunil-dhi <sunildhimanmatharu@gmail.com>
2023-08-18 20:02:02 -0400

    group anagram

---
+++ leetCodeJs/string_problem3.js
@@ -0,0 +1,12 @@
+///check group of anagram in array
+var groupAnagrams = function(strs) {
+    let map = {}
+
+    for(let str of strs){
+        let s = str.split('').sort().join('')
+        if(!map[s]) map[s] = []
+        map[s].push(str)
+    }
+    return Object.values(map)
+};
+console.log(groupAnagrams(['ate','eat','rat','tar','rta','man']))
